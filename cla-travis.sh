596e97429f969641a2f305aecb2224166589f4dd
Michael Ficarra <mficarra@shapesecurity.com>
2016-03-09 14:27:59 -0800

    allow github no-reply emails for contributor github accounts

--- cla-travis.sh
+++ cla-travis.sh
@@ -2,18 +2,23 @@
 
 CLA_CSV_URL="https://raw.githubusercontent.com/shapesecurity/CLA/master/CONTRIBUTORS.csv"
 CLA_URL="https://github.com/shapesecurity/CLA"
+if [ $# -gt 0 ]; then
+  CSV_DATA=`cat "$1" | tail -n +2`
+else
+  CSV_DATA=(`curl "$CLA_CSV_URL" 2>/dev/null | tail -n +2`)
+fi
 
-COMMITTERS=(`git log --pretty=format:"%ae%n%ce" $TRAVIS_COMMIT_RANGE | sort -u`)
+COMMITTERS=(`git log --pretty=format:"%ae%n%ce" "$TRAVIS_COMMIT_RANGE" | sort -u`)
 
 echo "Committers in this range: ${COMMITTERS[@]}"
 echo
 
-CONTRIBUTORS=(`curl $CLA_CSV_URL 2>/dev/null | awk -F, '/,/{gsub(/ /, "", $0); print $3}'`)
+CONTRIBUTORS=(`echo "$CSV_DATA" | awk -F, '/,/{gsub(/ /, "", $0); print $2 "@users.noreply.github.com"; print $4}'`)
 
 l2=" ${CONTRIBUTORS[*]} "
 for item in ${COMMITTERS[@]}; do
   CONTRIBUTOR=false
-  if [[ $l2 =~ " $item " ]] ; then
+  if [[ "$l2" =~ " $item " ]] ; then
     CONTRIBUTOR=true
   fi
   if !($CONTRIBUTOR); then
